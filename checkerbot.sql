41248b03482014fa6f0cee19b9c10510127fbdf7
mskthapa <87113529+mskthapa@users.noreply.github.com>
2021-10-29 18:48:57 +0545

    Update checkerbot.sql

--- checkerbot.sql
+++ checkerbot.sql
@@ -61,36 +61,3 @@ CREATE TABLE `users` (
   `total_cvv` varchar(50) NOT NULL,
   `total_ccn` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `antispam`
---
-ALTER TABLE `antispam`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `antispam`
---
-ALTER TABLE `antispam`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
-COMMIT;
