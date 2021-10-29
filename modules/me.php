6685302647b0276e4719525515080b56be3ec6b1
mskthapa <87113529+mskthapa@users.noreply.github.com>
2021-10-29 20:55:57 +0545

    Update me.php

--- modules/me.php
+++ modules/me.php
@@ -23,7 +23,7 @@ if(strpos($message, "/me") === 0 || strpos($message, "!me") === 0){
     $antispam = antispamCheck($userId);
     addUser($userId);
     
-    if($antispam != False){
+    if($antispam != True){
       bot('sendmessage',[
         'chat_id'=>$chat_id,
         'text'=>"[<u>ANTI SPAM</u>] Try again after <b>$antispam</b>s.",
